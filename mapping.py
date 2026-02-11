d7a77622c391854d7912e82c46efa39da1bf509a
Emanuel085 <carlosemanuel2203@gmail.com>
2026-02-11 14:31:16 -0300

    Ajuste final

--- mapping.py
+++ mapping.py
@@ -111,7 +111,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 #random forest
 print("Treinando o modelo")
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight="balanced")
 model.fit(X_train, y_train)
 
 #avaliando modelo
